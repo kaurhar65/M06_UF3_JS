af374c1315bd35d439f33f316df9740987f79d61
Harpreet Kaur <hkaur22@jaumebalmes.net>
2024-02-19 23:47:04 +0100

    .

--- dragAndDrop/script.js
+++ dragAndDrop/script.js
@@ -1,1 +1,16 @@
 let arrayFitxers=[];
+let files;
+const dropArea = document.querySelector('.dropArea');
+const dragDropText = document.querySelector('h2');
+const button = document.querySelector('button');
+const input = document.querySelector('#input-file');
+const preview = document.querySelector('#preview');
+
+
+button.addEventListener("click", () =>{
+  input.click();
+});
+
+input.addEventListener("change", () => {
+  files= this.files;
+})
